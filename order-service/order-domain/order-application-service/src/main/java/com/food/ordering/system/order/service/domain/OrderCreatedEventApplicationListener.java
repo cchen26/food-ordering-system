730a52c2b1050cb00e2613cfc2dbe73a4bb592a5
Chao Chen <chaochen234@gmail.com>
2024-12-02 22:04:40 -0500

    Publish with transactional listener

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreatedEventApplicationListener.java
@@ -0,0 +1,31 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment.OrderCreatedPaymentRequestMessagePublisher;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+/**
+ * @author Chao
+ * @version 1.0
+ * @email chaochen234@gmail.com
+ * @since 2024-12-02
+ */
+
+@Slf4j
+@Component
+public class OrderCreatedEventApplicationListener {
+
+    private final OrderCreatedPaymentRequestMessagePublisher orderCreatedPaymentRequestMessagePublisher;
+
+    public OrderCreatedEventApplicationListener(OrderCreatedPaymentRequestMessagePublisher
+                                                        orderCreatedPaymentRequestMessagePublisher) {
+        this.orderCreatedPaymentRequestMessagePublisher = orderCreatedPaymentRequestMessagePublisher;
+    }
+
+    @TransactionalEventListener
+    void process(OrderCreatedEvent orderCreatedEvent) {
+        orderCreatedPaymentRequestMessagePublisher.publish(orderCreatedEvent);
+    }
+}
